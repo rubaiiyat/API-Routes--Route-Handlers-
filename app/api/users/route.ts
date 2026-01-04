638ad6c11e5394dc1e80ee7fec41a80a602f1908
rubaiiyat <abirr1516@gmail.com>
2026-01-04 23:47:25 +0600

    handling post request

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,16 @@
 import { data } from "./data";
 
-export async function GET(request: Request) {
-  console.log(request);
+export async function GET() {
   return Response.json(data);
+}
+
+export async function POST(request: Request) {
+  const newData = await request.json();
+
+  const newUser = {
+    id: data.length + 1,
+    ...newData,
+  };
+  data.push(newUser);
+  return new Response(JSON.stringify(newUser), { status: 201 });
 }
