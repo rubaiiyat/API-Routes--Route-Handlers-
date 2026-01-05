ab47bb1ec302876c74f6fee4f3183ec48dddd83a
rubaiiyat <abirr1516@gmail.com>
2026-01-05 22:06:54 +0600

    url query parameters

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,18 @@
+import { NextRequest } from "next/server";
 import { data } from "./data";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const search = searchParams.get("search");
+
+  if (search) {
+    const filterData = data.filter(
+      (user) =>
+        user.name.toLowerCase().includes(search.toLowerCase()) ||
+        user.email.toLowerCase().includes(search.toLowerCase())
+    );
+    return Response.json(filterData);
+  }
   return Response.json(data);
 }
 
