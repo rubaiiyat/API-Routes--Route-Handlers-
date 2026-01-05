55332b6a49ae28b6e344344126352641bc587121
rubaiiyat <abirr1516@gmail.com>
2026-01-05 19:47:18 +0600

    handling patch request

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -13,3 +13,22 @@ export async function GET(
     return new Response("User not found", { status: 404 });
   }
 }
+
+// Update User by ID
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  const updateData = await request.json();
+  const userIndex = data.findIndex((user) => user.id === parseInt(id));
+
+  if (userIndex === -1) {
+    return new Response("User not found", { status: 404 });
+  } else {
+    const updateUser = { ...data[userIndex], ...updateData };
+    data[userIndex] = updateUser;
+    return Response.json(updateUser);
+  }
+}
